04b4a1fdc00804d94ad526d72935f7b0750da209
Harsh Saini <harshsaini0107@gmail.com>
2025-02-08 23:45:12 +0530

    adding files

---
+++ src/BlackJack.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class BlackJack {
+
+    private class Card{
+
+        String value;
+        String type;
+
+        Card(String value, String type){
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    BlackJack (){
+        startGame();
+    }
+
+    public void startGame(){
+
+    }
+}
